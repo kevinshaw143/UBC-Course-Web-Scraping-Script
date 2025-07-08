5fdfd7023692d232b3ed696b6a4b8c7b9e280041
kshaw031 <kevin.shaw143@gmail.com>
2025-07-07 21:47:17 -0700

    Script now gets course codes and descriptions. Prints them

--- Program.cs
+++ Program.cs
@@ -10,20 +10,6 @@ namespace UBC_Course_Web_Scrapping_Script
 {
     internal class Program
     {
-
-        static void printLinks(IEnumerable<string> links)
-        {
-            foreach (var link in links)
-            {
-                Console.WriteLine(link);
-            }
-        }
-
-        static void parseLinkedPages(IEnumerable<string> links)
-        {
-            Console.WriteLine("hi");
-        }
-
         static void Main(string[] args)
         {
             var url = "https://vancouver.calendar.ubc.ca/course-descriptions/courses-subject";
@@ -35,9 +21,41 @@ namespace UBC_Course_Web_Scrapping_Script
 
             IEnumerable<string> links = hrefs.Where(link => link.Contains("https://vancouver.calendar.ubc.ca/course-descriptions/subject/"));
 
-            printLinks(links);
-
             parseLinkedPages(links);
+        }
+
+        static void printLinks(IEnumerable<string> links)
+        {
+            foreach (var link in links)
+            {
+                Console.WriteLine(link);
+            }
+        }
+
+        static void parseLinkedPages(IEnumerable<string> links)
+        {
+            foreach (string link in links)
+            {
+                parseLinkedPage(link);
+            }
+        }
+
+        static void parseLinkedPage(string link)
+        {
+            var url = link;
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+
+            var courseTags = doc.DocumentNode.SelectNodes("//div[contains(@class, 'flow-root text-formatted node__content relative')]");
+
+            foreach (var courseTag in courseTags)
+            {
+                string courseCodeInfo = courseTag.Descendants("h3").First().InnerHtml;
+                string courseDescription = courseTag.Descendants("p").First().InnerHtml;
+
+                Console.WriteLine("Course Code Info: " + courseCodeInfo);
+                Console.WriteLine("Course Description: " + courseDescription);
+            }
         }
     }
 }
